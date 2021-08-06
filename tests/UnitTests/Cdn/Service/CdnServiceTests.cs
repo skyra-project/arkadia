b1e738127dbc01bac3f5b0e7c3b0c4a81f50e28b
Tyler Davis <tgfdavis98@gmail.com>
2021-08-06 11:43:36 +0100

    add more tests.

--- tests/UnitTests/Cdn/Service/CdnServiceTests.cs
+++ tests/UnitTests/Cdn/Service/CdnServiceTests.cs
@@ -25,7 +25,7 @@ namespace UnitTests.Cdn.Service
 
 			Environment.SetEnvironmentVariable("BASE_ASSET_LOCATION", "/assets");
 
-			var repository = new MockCdnRepository();
+			await using var repository = new MockCdnRepository();
 			
 			var factory = new MockCdnRepositoryFactory(repository);
 			var fileSystem = new MockFileSystem();
@@ -56,6 +56,71 @@ namespace UnitTests.Cdn.Service
 			
 			Assert.AreEqual(CdnResult.Ok, returnValue.Result);
 			Assert.AreEqual(content, returnValue.Content.ToByteArray());
+		}
+		
+		[Test]
+		public async Task CdnService_Get_ShouldReturnDoesNotExist_WhenDoesNotExist()
+		{
+			
+			// arrange
+
+			Environment.SetEnvironmentVariable("BASE_ASSET_LOCATION", "/assets");
+
+			await using var repository = new MockCdnRepository();
+			
+			var factory = new MockCdnRepositoryFactory(repository);
+			var fileSystem = new MockFileSystem();
+			var service = new CdnService(new NullLogger<CdnService>(), fileSystem, factory);
+			
+
+			const string entryName = "test1";
+
+			var query = new GetRequest
+			{
+				Name = entryName
+			};
+			
+			// act
+			
+			var returnValue = await service.Get(query, null!);
+			
+			// assert
+			
+			Assert.AreEqual(CdnResult.DoesNotExist, returnValue.Result);
+		}
+		
+		[Test]
+		public async Task CdnService_Get_ShouldReturnError_WhenExists_ButFileDoesNot()
+		{
+			
+			// arrange
+
+			Environment.SetEnvironmentVariable("BASE_ASSET_LOCATION", "/assets");
+
+			await using var repository = new MockCdnRepository();
+			
+			var factory = new MockCdnRepositoryFactory(repository);
+			var fileSystem = new MockFileSystem();
+			var service = new CdnService(new NullLogger<CdnService>(), fileSystem, factory);
+			
+
+			const string entryName = "test1";
+
+			var query = new GetRequest
+			{
+				Name = entryName
+			};
+			
+			// act
+
+			var entity = await repository.UpsertEntryAsync(entryName, "test/default", "test_tag", DateTime.Now);
+			var id = entity.Id;
+
+			var returnValue = await service.Get(query, null!);
+			
+			// assert
+			
+			Assert.AreEqual(CdnResult.Error, returnValue.Result);
 		}
 	}
 }
